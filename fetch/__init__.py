e3467af62ccd565fcd3c13a0007fe6c27cdcfaba
Boripath <yeenboripath@gmail.com>
2025-07-27 23:02:21 +0700

    Update __init__.py

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -2,6 +2,6 @@
 # ไว้สำหรับรวมการโหลดโมดูลภายในโฟลเดอร์ fetch
 
 from .fetch_deribit import fetch_deribit
-from .fetch_binance import fetch_binance
-from .fetch_bybit import fetch_bybit
+from .fetch_binance import fetch_data as fetch_binance
+from .fetch_bybit import fetch_data as fetch_bybit
 
